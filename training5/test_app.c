17074cdbe70a685f3886ee7959e7b55ba9773459
JGH <siblue202@gmail.com>
2022-04-29 16:02:34 +0900

    modify code

--- training5/test_app.c
+++ training5/test_app.c
@@ -26,8 +26,6 @@ int main(int argc, char **argv)
 	memcpy(get_number, argv[1], sizeof(get_number));
 
 	write(test_fd, &get_number, sizeof(get_number));
-	read(test_fd, &rst, sizeof(rst));
-	printf("read data from kernel buffer : %d \n", rst);
 	close(test_fd);
 
 	return 0;
