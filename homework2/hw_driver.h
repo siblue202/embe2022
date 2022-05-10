a254fa8f6debfefedb75d27841b0a10ac78323cb
JGH <siblue202@gmail.com>
2022-05-10 17:43:38 +0900

    modify file

--- homework2/hw_driver.h
+++ homework2/hw_driver.h
@@ -9,7 +9,7 @@ typedef struct Ioctl_info{
 }Ioctl_info;
    
 #define             IOCTL_MAGIC         'T'
-#define             SET_OPTION          _IOWR(IOCTL_MAGIC, 2 ,struct ioctl_info)
-#define             COMMAND             _IOWR(IOCTL_MAGIC, 3 ,struct ioctl_info)
+#define             SET_OPTION          _IOWR(IOCTL_MAGIC, 2 ,struct Ioctl_info)
+#define             COMMAND             _IOWR(IOCTL_MAGIC, 3 ,struct Ioctl_info)
   
 #endif
