e9f0ef3598c38610e425e4ff7a63dd7eb2994738
JGH <siblue202@gmail.com>
2022-05-15 20:44:36 +0900

    modify file

--- homework2/hw_driver.c
+++ homework2/hw_driver.c
@@ -240,8 +240,8 @@ static void kernel_timer_function(unsigned long data) {
 		specific_data = value[index_value];
 		memcpy(p_data->value, &value, sizeof(value));
 
-		shift_text(line_1);
-		shift_text(line_2);
+		shift_text(&line_1);
+		shift_text(&line_2);
 		memcpy(string_lcd, line_1, LINE_BUFF);
 		memcpy(string_lcd+LINE_BUFF, line_2, LINE_BUFF);
 
