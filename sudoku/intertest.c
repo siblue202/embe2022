1ec9df883b769070571058afe4f3a025356731a4
JGH <siblue202@gmail.com>
2022-06-20 06:59:51 +0900

    add sudoku

--- sudoku/intertest.c
+++ sudoku/intertest.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <string.h>
 
 unsigned char quit = 0;
 
@@ -13,6 +14,12 @@ void user_signal1(int sig){
 int main(void){
 	int fd;
 	char buf;
+    int result;
+
+    char cmp_1 = '1';
+    char cmp_2 = '2';
+    char cmp_3 = '3';
+    char cmp_4 = '4';
 
 	fd = open("/dev/inter", O_RDWR);
 	if(fd < 0) {
@@ -27,7 +34,20 @@ int main(void){
         usleep(400000);
 
         read(fd, &buf, 1);
-        printf("read result : %c\n", buf);
+        //printf("read result : %c\n", buf);
+        if (strcmp(&buf,&cmp_1) == 0){
+            result = 1;
+        } else if (strcmp(&buf,&cmp_2) == 0){
+            result = 2;
+        } else if (strcmp(&buf,&cmp_3) == 0){
+            result = 3;
+        } else if (strcmp(&buf,&cmp_4) == 0){
+            result = 4;
+        } else {
+            result = 0;
+        }
+
+        printf("read result : %d\n", result);
 
     }
 
