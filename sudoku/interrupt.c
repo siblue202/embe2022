49951d280715075746b2c0714080f47ae9b48a2e
JGH <siblue202@gmail.com>
2022-06-20 06:32:36 +0900

    add sudoku

--- sudoku/interrupt.c
+++ sudoku/interrupt.c
@@ -44,34 +44,34 @@ static struct file_operations inter_fops =
 };
 
 irqreturn_t inter_handler1(int irq, void* dev_id, struct pt_regs* reg) {
-	printk(KERN_ALERT "interrupt1!!! = %x\n", gpio_get_value(IMX_GPIO_NR(1, 11)));
+	//printk(KERN_ALERT "interrupt1!!! = %x\n", gpio_get_value(IMX_GPIO_NR(1, 11)));
 
 	return_value = '1';
-    printk(KERN_ALERT "return_value : %c\n", return_value);
+    //printk(KERN_ALERT "return_value : %c\n", return_value);
 	return IRQ_HANDLED;
 }
 
 irqreturn_t inter_handler2(int irq, void* dev_id, struct pt_regs* reg) {
-    printk(KERN_ALERT "interrupt2!!! = %x\n", gpio_get_value(IMX_GPIO_NR(1, 12)));
+    //printk(KERN_ALERT "interrupt2!!! = %x\n", gpio_get_value(IMX_GPIO_NR(1, 12)));
         
     return_value = '2';
-    printk(KERN_ALERT "return_value : %c\n", return_value);
+    //printk(KERN_ALERT "return_value : %c\n", return_value);
     return IRQ_HANDLED;
 }
 
 irqreturn_t inter_handler3(int irq, void* dev_id,struct pt_regs* reg) {
-    printk(KERN_ALERT "interrupt3!!! = %x\n", gpio_get_value(IMX_GPIO_NR(2, 15)));
+    //printk(KERN_ALERT "interrupt3!!! = %x\n", gpio_get_value(IMX_GPIO_NR(2, 15)));
         
     return_value = '3';
-    printk(KERN_ALERT "return_value : %c\n", return_value);
+    //printk(KERN_ALERT "return_value : %c\n", return_value);
 	return IRQ_HANDLED;
 }
 
 irqreturn_t inter_handler4(int irq, void* dev_id, struct pt_regs* reg) {
-    printk(KERN_ALERT "interrupt4!!! = %x\n", gpio_get_value(IMX_GPIO_NR(5, 14)));
+    //printk(KERN_ALERT "interrupt4!!! = %x\n", gpio_get_value(IMX_GPIO_NR(5, 14)));
 		
     return_value = '4';
-    printk(KERN_ALERT "return_value : %c\n", return_value);
+    //printk(KERN_ALERT "return_value : %c\n", return_value);
     return IRQ_HANDLED;
 }
 
@@ -80,30 +80,30 @@ static int inter_open(struct inode *minode, struct file *mfile){
 	int ret;
 	int irq;
 
-	printk(KERN_ALERT "Open Module\n");
+	//printk(KERN_ALERT "Open Module\n");
 
 	// int1
 	gpio_direction_input(IMX_GPIO_NR(1,11));
 	irq = gpio_to_irq(IMX_GPIO_NR(1,11));
-	printk(KERN_ALERT "IRQ Number : %d\n",irq);
+	//printk(KERN_ALERT "IRQ Number : %d\n",irq);
 	ret=request_irq(irq, inter_handler1, IRQF_TRIGGER_FALLING, "home", 0);
 
 	// int2
 	gpio_direction_input(IMX_GPIO_NR(1,12));
 	irq = gpio_to_irq(IMX_GPIO_NR(1,12));
-	printk(KERN_ALERT "IRQ Number : %d\n",irq);
+	//printk(KERN_ALERT "IRQ Number : %d\n",irq);
 	ret=request_irq(irq, inter_handler2, IRQF_TRIGGER_FALLING, "back", 0);
 
 	// int3
 	gpio_direction_input(IMX_GPIO_NR(2,15));
 	irq = gpio_to_irq(IMX_GPIO_NR(2,15));
-	printk(KERN_ALERT "IRQ Number : %d\n",irq);
+	//printk(KERN_ALERT "IRQ Number : %d\n",irq);
 	ret=request_irq(irq, inter_handler3, IRQF_TRIGGER_RISING, "volup", 0);
 
 	// int4
 	gpio_direction_input(IMX_GPIO_NR(5,14));
 	irq = gpio_to_irq(IMX_GPIO_NR(5,14));
-	printk(KERN_ALERT "IRQ Number : %d\n",irq);
+	//printk(KERN_ALERT "IRQ Number : %d\n",irq);
 	ret=request_irq(irq, inter_handler4, IRQF_TRIGGER_FALLING, "voldown", 0);
 
 	return 0;
@@ -115,16 +115,16 @@ static int inter_release(struct inode *minode, struct file *mfile){
 	free_irq(gpio_to_irq(IMX_GPIO_NR(2, 15)), NULL);
 	free_irq(gpio_to_irq(IMX_GPIO_NR(5, 14)), NULL);
 	
-	printk(KERN_ALERT "Release Module\n");
+	// printk(KERN_ALERT "Release Module\n");
 	return 0;
 }
 
 static int inter_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos ){
 	if(interruptCount==0){
-                printk("sleep on\n");
+                //printk("sleep on\n");
                 interruptible_sleep_on(&wq_write);
          }
-	printk("write\n");
+	//printk("write\n");
 	return 0;
 }
 
@@ -167,15 +167,15 @@ static int __init inter_init(void) {
 	int result;
 	if((result = inter_register_cdev()) < 0 )
 		return result;
-	printk(KERN_ALERT "Init Module Success \n");
-	printk(KERN_ALERT "Device : /dev/inter, Major Num : 246 \n");
+	//printk(KERN_ALERT "Init Module Success \n");
+	//printk(KERN_ALERT "Device : /dev/inter, Major Num : 246 \n");
 	return 0;
 }
 
 static void __exit inter_exit(void) {
 	cdev_del(&inter_cdev);
 	unregister_chrdev_region(inter_dev, 1);
-	printk(KERN_ALERT "Remove Module Success \n");
+	//printk(KERN_ALERT "Remove Module Success \n");
 }
 
 module_init(inter_init);
